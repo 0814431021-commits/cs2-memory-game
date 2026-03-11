35a56ae673360875c37808f3bf65bbd54da5f6fd
0814431021-commits <0814431021@s.tusd1.org>
2026-03-11 18:53:52 +0000

    done

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 console.log("Script started");
+
+let guesses = 0;
+let firstCardId = "";
+let secondCardId = "";
+// Flip card to reveal the text
+function flipCard(cardId) {
+    // print click to console to make sure its working
+    console.log("click");
+    // Get the card by its id
+    let card1 = document.getElementById(cardId);
+    // Changes the color to reveal the text of the cards
+    card1.style.color =  "pink";
+    card1.style.backroundcolor = "darkred";
+    // check if this is their first click
+    if (firstCardId == ""){
+        // Store first card id
+        firstCardId = cardId;
+    }
+    else {
+        //Store the second Id
+        secondCardId = cardId;
+    }
+
+}
+
+function flipCard(cardId) {
+    // Check
+}
